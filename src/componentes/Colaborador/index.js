f290aefd58c5c7659d651b1b7956b2aaf332a23e
Andre Palhares <andrepalhares@hotmail.com>
2023-03-07 20:57:09 -0300

    implementando exclusão de colaborador e input de cores

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,10 @@
+import { AiFillCloseCircle } from 'react-icons/ai';
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
+        <AiFillCloseCircle size={23} className="deletar" onClick={() => aoDeletar(colaborador.id)} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,13 +1,14 @@
+import hexToRgba from 'hex-to-rgba'
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, aoDeletarColaborador, alterarCor }) => {
     return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: `${hexToRgba(time.cor, '0.2')}` }}>
+            <input type='color' value={time.cor} onChange={(event) => alterarCor(event.target.value, time.id)} className='input-cor' />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+                {colaboradores.map((colaborador, indice) => <Colaborador aoDeletar={aoDeletarColaborador} key={indice} colaborador={colaborador} corDeFundo={time.cor} />)}
             </div>
         </section>
 
